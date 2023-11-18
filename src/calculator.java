213025e430e1244cba87c41d86d525514369f4cb
unknown <saianjan1234@gmail.com>
2023-11-18 10:08:31 +0530

    Added the Menu based approcah to the Program

--- src/calculator.java
+++ src/calculator.java
@@ -16,20 +16,24 @@ public class calculator {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         double var1,var2;
-        int input;
-        System.out.println("Welcome to Calculator App!");
-        System.out.println("Enter 1st number:");
-        var1=sc.nextDouble();
-        System.out.println("Enter 2nd number:");
-        var2=sc.nextDouble();
-        System.out.println("Enter a choice\n 1:For Addition\n 2:For Subtraction\n 3:For Multiplication\n 4:For Division");
-        input=sc.nextInt();
-        switch (input) {
-            case 1 -> System.out.println("Addition of the two number is " + add(var1,var2));
-            case 2 -> System.out.println("Subtraction of the two numbers is " + subtract(var1,var2));
-            case 3 -> System.out.println("Multiplication of the two numbers is " + multiply(var1,var2));
-            case 4 -> System.out.println("Division of the two numbers is " + divide(var1,var2));
-            default -> System.out.println("Invalid Input!");
-        }
+        int input,option=0;
+        do {
+            System.out.println("Welcome to Calculator App!");
+            System.out.println("Enter 1st number:");
+            var1 = sc.nextDouble();
+            System.out.println("Enter 2nd number:");
+            var2 = sc.nextDouble();
+            System.out.println("Enter a choice\n 1:For Addition\n 2:For Subtraction\n 3:For Multiplication\n 4:For Division");
+            input = sc.nextInt();
+            switch (input) {
+                case 1 -> System.out.println("Addition of the two number is " + add(var1, var2));
+                case 2 -> System.out.println("Subtraction of the two numbers is " + subtract(var1, var2));
+                case 3 -> System.out.println("Multiplication of the two numbers is " + multiply(var1, var2));
+                case 4 -> System.out.println("Division of the two numbers is " + divide(var1, var2));
+                default -> System.out.println("Invalid Input!");
+            }
+            System.out.println("To Exit the Program Press 1 or Press any key to continue.");
+            option = sc.nextInt();
+        }while (option!=1);
     }
 }
