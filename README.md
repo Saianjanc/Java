03a8199552d7f08b8fc0f0daa2fa7e6a8eebfc7b
unknown <saianjan1234@gmail.com>
2023-11-18 10:13:52 +0530

    Segregated the functionalities

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-Calculator App with Basic Funtionalities! 
+calculator App with Basic Funtionalities! 
 The Appliction takes input from the user. 
 And then computes the addition of the two numbers. 
 Displays the result sum of the numbers.
-*Added Subtraction feature to the Calculator App.
+*Added Subtraction feature to the calculator App.
 Computes the subtraction of the numbers and displays the result.
-*Added Multiplication feature to the Calculator App.
+*Added Multiplication feature to the calculator App.
 Computes the Multiplication of the numbers and displays the result.
-*Added Division feature to the Calculator App.
+*Added Division feature to the calculator App.
 Computes th division of the two numbers and displays the result.
--- src/calculator.java
+++ src/calculator.java
@@ -1,22 +1,11 @@
 import java.util.Scanner;
 
 public class calculator {
-    public static double add(double a, double b){
-        return a+b;
-    }
-    public static double subtract(double a, double b){
-        return a-b;
-    }
-    public static double multiply(double a, double b){
-        return a*b;
-    }
-    public static double divide(double a, double b){
-        return a/b;
-    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         double var1,var2;
         int input,option=0;
+        Functions f = new Functions();
         do {
             System.out.println("Welcome to Calculator App!");
             System.out.println("Enter 1st number:");
@@ -26,10 +15,10 @@ public class calculator {
             System.out.println("Enter a choice\n 1:For Addition\n 2:For Subtraction\n 3:For Multiplication\n 4:For Division");
             input = sc.nextInt();
             switch (input) {
-                case 1 -> System.out.println("Addition of the two number is " + add(var1, var2));
-                case 2 -> System.out.println("Subtraction of the two numbers is " + subtract(var1, var2));
-                case 3 -> System.out.println("Multiplication of the two numbers is " + multiply(var1, var2));
-                case 4 -> System.out.println("Division of the two numbers is " + divide(var1, var2));
+                case 1 -> System.out.println("Addition of the two number is " + f.add(var1, var2));
+                case 2 -> System.out.println("Subtraction of the two numbers is " + f.subtract(var1, var2));
+                case 3 -> System.out.println("Multiplication of the two numbers is " + f.multiply(var1, var2));
+                case 4 -> System.out.println("Division of the two numbers is " + f.divide(var1, var2));
                 default -> System.out.println("Invalid Input!");
             }
             System.out.println("To Exit the Program Press 1 or Press any key to continue.");
